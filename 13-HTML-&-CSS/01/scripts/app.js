4086e88943e340cb11ab020a3ccb9b4989908a76
VioletaKirova <violetakirova1@gmail.com>
2020-01-14 19:53:31 +0200

    Changed the HTML structure and animated the hero image

--- 13-HTML-&-CSS/01/scripts/app.js
+++ 13-HTML-&-CSS/01/scripts/app.js
@@ -1,37 +1,58 @@
 const html = {
-  nav: () => document.querySelector('nav'),
-  headline: () => document.querySelector('#headline'),
-  subHeadline: () => document.querySelector('#sub-headline'),
-  hamburgerIcon: () => document.querySelector('.nav-hamburger-menu-icon'),
-  dropDownMenu: () => document.querySelector('.nav-hamburger-menu-container')
-}
+  nav: () => document.getElementById('nav'),
+  headline: () => document.getElementById('headline'),
+  subHeadline: () => document.getElementById('sub-headline'),
+  imgBack: () => document.getElementById('img-back'),
+  imgFront: () => document.getElementById('img-front'),
+  hamburgerIcon: () => document.getElementById('ham-icon'),
+  dropDownMenu: () => document.getElementById('ham-menu')
+};
 
 const tl = new TimelineMax();
 
 tl.fromTo(html.nav(), 1, { y: '-100%' }, { y: '0%', ease: Power2.easeInOut })
-  .fromTo(html.headline(), 1.4, { opacity: 0 }, { opacity: 1 })
+  .fromTo(
+    html.imgBack(),
+    1.8,
+    { y: '-10%' },
+    { y: '0%', ease: Power2.easeInOut }
+  )
+  .fromTo(
+    html.imgFront(),
+    1.8,
+    { y: '-10%' },
+    { y: '0%', ease: Power2.easeInOut },
+    '-=1.8'
+  )
+  .fromTo(html.headline(), 1.8, { opacity: 0 }, { opacity: 1 }, '-=1.8')
   .fromTo(
     html.headline(),
-    1.2,
+    1.6,
     { y: '100%' },
     { y: '-70%', ease: Power2.easeInOut },
-    '-=1.4'
+    '-=1.8'
   )
-  .fromTo(html.subHeadline(), 1.4, { opacity: 0 }, { opacity: 1 })
-  .fromTo(html.subHeadline(), 1.2, {x: '-100%'}, {x: '0%', ease: Power2.easeInOut}, '-=1.4');
+  .fromTo(html.subHeadline(), 1.8, { opacity: 0 }, { opacity: 1 }, '-=1.8')
+  .fromTo(
+    html.subHeadline(),
+    1.8,
+    { y: '550%' },
+    { y: '0%', ease: Power2.easeInOut },
+    '-=1.8'
+  );
 
 function showDropDownMenu() {
-  html.dropDownMenu().style.display = "flex";
+  html.dropDownMenu().style.display = 'flex';
 
-  html.hamburgerIcon().removeEventListener("click", showDropDownMenu);
-  html.hamburgerIcon().addEventListener("click", hideDropDownMenu);
+  html.hamburgerIcon().removeEventListener('click', showDropDownMenu);
+  html.hamburgerIcon().addEventListener('click', hideDropDownMenu);
 }
 
 function hideDropDownMenu() {
-  html.dropDownMenu().style.display = "none";
+  html.dropDownMenu().style.display = 'none';
 
-  html.hamburgerIcon().removeEventListener("click", hideDropDownMenu);
-  html.hamburgerIcon().addEventListener("click", showDropDownMenu);
+  html.hamburgerIcon().removeEventListener('click', hideDropDownMenu);
+  html.hamburgerIcon().addEventListener('click', showDropDownMenu);
 }
 
 html.hamburgerIcon().addEventListener('click', showDropDownMenu);
